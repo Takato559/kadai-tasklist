46da8930e4b60eec4fd0da6c36674891c269e638
kaykay <kdotjelly@gmail.com>
2020-08-02 13:16:08 +0900

    Add UpdateServlet

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -1,19 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Tasklist;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class UpdateServlet
  */
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +28,36 @@ public class UpdateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String _token = (String)request.getParameter("_token");
+
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Tasklist t = em.find(Tasklist.class, (Integer)(request.getSession().getAttribute("task_id")));
+                                                                                //from EditServlet(t.getId())
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime);
+
+            //DB更新
+            em.getTransaction().begin();
+//            em.persist(t);
+            em.getTransaction().commit();
+            em.close();
+
+            //post -> get -> set -> redirect
+            request.getSession().removeAttribute("task_id");
+            response.sendRedirect(request.getContextPath() + "/index");
+
+        }
+    }
 
 }
